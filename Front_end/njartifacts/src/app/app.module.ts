1aafe37a3590ec70f5dd31f64f1ce7fe45deb76a
NilsonVargas <59322799+NilsonVargas@users.noreply.github.com>
2021-10-23 21:39:59 -0500

    Se agregaron las liberias de bootstrap y jquary, y el componente information

--- Front_end/njartifacts/src/app/app.module.ts
+++ Front_end/njartifacts/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InformationComponent } from './components/information/information.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InformationComponent
   ],
   imports: [
     BrowserModule,
