cf7792cf58cae33e0a5fd040750205f7040e370e
NilsonVargas <59322799+NilsonVargas@users.noreply.github.com>
2021-10-31 14:56:55 -0500

    Interfaz product definida

--- Front_end/njartifacts/src/app/interfaces/product.ts
+++ Front_end/njartifacts/src/app/interfaces/product.ts
@@ -1,3 +1,9 @@
 export interface Product {
-    
+    id: number;
+    name_product: string;
+    description: string;
+    categoryId: number;
+    list_features: [];
+    url_image: string;
+    value: number;
 }
