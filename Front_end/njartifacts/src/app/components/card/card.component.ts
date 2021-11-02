32ef81ee13224577caf2f3bb49b2262b194fe51e
NilsonVargas <59322799+NilsonVargas@users.noreply.github.com>
2021-11-02 15:39:34 -0500

    Se agrego la una forma de comprobar si existe un componente con imagen

--- Front_end/njartifacts/src/app/components/card/card.component.ts
+++ Front_end/njartifacts/src/app/components/card/card.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CardComponent implements OnInit {
   @Input() name_product:string = "Asus ASROCK";
-  @Input() image_src:string = "../../assets/images/TRX40 Creator.png";
+  @Input() image_src:string = "../../assets/images/placeholder-large.jpg";
   @Input() description:string = "This a fucking description";
   category:string = "Motherboard";
   @Input() quantity:number = 10;
