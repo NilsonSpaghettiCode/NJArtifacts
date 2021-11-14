641ed71eed06801efedf75a74a257b944309f411
NilsonVargas <59322799+NilsonVargas@users.noreply.github.com>
2021-11-13 22:21:42 -0500

    Se agrego una tabla para almacenar imagenes

--- Back_end/njartifacts/app/Models/Imagen.php
+++ Back_end/njartifacts/app/Models/Imagen.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Imagen extends Model
 {
     use HasFactory;
+
+    protected $table = "imagens";
 }
--- Back_end/njartifacts/database/migrations/2021_11_14_021659_create_imagens_table.php
+++ Back_end/njartifacts/database/migrations/2021_11_14_021659_create_imagens_table.php
@@ -15,7 +15,11 @@ class CreateImagensTable extends Migration
     {
         Schema::create('imagens', function (Blueprint $table) {
             $table->id();
+            $table->string('url_imagen');
+            $table->unsignedBigInteger('id_productopk');
+            $table->foreign('id_productopk')->references('id_producto')->on('producto');
             $table->timestamps();
+            
         });
     }
 
